335aed2803d5f3372dae69c22e0d4dd4d2dab38c
Mickael Tits <mickaeltits@gmail.com>
2015-08-13 18:12:17 +0200

    ofxUI debugged

--- libs/oF/addons/ofxUI/src/ofxUITextArea.cpp
+++ libs/oF/addons/ofxUI/src/ofxUITextArea.cpp
@@ -1,26 +1,26 @@
 /**********************************************************************************
- 
- Copyright (C) 2012 Syed Reza Ali (www.syedrezaali.com)
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy of
- this software and associated documentation files (the "Software"), to deal in
- the Software without restriction, including without limitation the rights to
- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- of the Software, and to permit persons to whom the Software is furnished to do
- so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
- 
- **********************************************************************************/
+
+Copyright (C) 2012 Syed Reza Ali (www.syedrezaali.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+**********************************************************************************/
 
 #include "ofxUITextArea.h"
 #include "ofxUI.h"
@@ -35,17 +35,17 @@ void ofxUITextArea::init(string _name, string _textstring, float w, float h, flo
     initRect(x,y,w,h);
     setName(_name);
     setKind(OFX_UI_WIDGET_TEXTAREA);
-    
+
     textstring = _textstring;
     setDrawFill(true);
     setDrawBack(false);
     drawShadow = false;
     drawLabel = false;
-    
+
     label = new ofxUILabel(padding*2.0,0,(name+" LABEL"), _size);
     addEmbeddedWidget(label);
     label->setVisible(drawLabel);
-    
+
     if(h == 0)
     {
         autoSize = true;
@@ -78,7 +78,7 @@ void ofxUITextArea::drawFill()
                 label->drawStringShadow(rect->getX(), rect->getY()+(lineHeight+lineSpaceSize)*(i+1)-lineSpaceSize, textLines[i]);
             }
         }
-        
+
         ofSetColor(color_fill);
         for(unsigned int i = 0; i < textLines.size(); i++)
         {
@@ -105,12 +105,12 @@ void ofxUITextArea::formatTextString()
     float rectWidthLimit = rect->getWidth()-padding*6;
     float rectHeightLimit = rect->getHeight()-label->getLineHeight()-padding;
     bool overheight = false;
-    
+
     lineHeight = label->getStringHeight("1");
     lineSpaceSize = padding*2;
-    
+
     offsetY = floor(padding*.125);
-    
+
     if(label->getStringWidth(textstring) <= rectWidthLimit)
     {
         if(textstring.size() > 0)
@@ -125,7 +125,7 @@ void ofxUITextArea::formatTextString()
         textLines.clear();
         string line = "";
         size_t i=0;
-        
+
         while (i < textstring.size() && !overheight) //if not at the end of the string && not over the rect's height
         {
             tempWidth = label->getStringWidth(line);
@@ -141,7 +141,7 @@ void ofxUITextArea::formatTextString()
             else
             {
                 bool notFound = true;
-                
+
                 while (notFound && !overheight)
                 {
                     if(strncmp(&textstring.at(i), " ",1) == 0)
@@ -163,21 +163,28 @@ void ofxUITextArea::formatTextString()
                             i++;
                         }
                     }
-                    else
+                    else if(i>0)
                     {
                         i--;
                         line.erase(line.end()-1);
+                    }
+                    else
+                    {
+                        //No space!
+                        overheight = true;
+                        textLines.clear();
+                        textLines.push_back(textstring);
                     }
                 }
             }
         }
     }
-    
+
     if(autoSize)
     {
         rect->setHeight((lineHeight+lineSpaceSize)*textLines.size()-lineSpaceSize);
     }
-    
+
     if(overheight)
     {
         rect->setHeight(MAX(rect->getHeight(),(lineHeight+lineSpaceSize)*textLines.size()-lineSpaceSize));
