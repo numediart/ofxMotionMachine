681239819aed0c42a54ddee0dace30331f6e8676
titsitits <mickaeltits@gmail.com>
2017-10-19 18:13:14 +0200

    Updated mmTutorial_3_TinyExample : display track center of mass in 2D layer instead of the first node of the track

--- apps/examples/mmTutorial_3_TinyExample/src/ofApp.cpp
+++ apps/examples/mmTutorial_3_TinyExample/src/ofApp.cpp
@@ -11,6 +11,7 @@ This is a short example showing how to enable the dragging of motion data in the
 */
 
 #include "ofApp.h"
+#include "mmGeometry.h"
 
 using namespace std;
 using namespace arma;
@@ -39,8 +40,11 @@ void ofApp::scene3d( void ) {
 void ofApp::scene2d( void ) {
     
     // Example to draw some timed data in the 2D scene
-    draw(track("1")(0).position, "First node position data"); // Draw the positions (x,y,z) of the first node (node 0)
-    //of the track "1"
+    //draw(track("1")(0).position, "First node position data"); // Draw the positions (x,y,z) of the first node (node 0) 
+                                                                //of the track "1"
+
+    draw(COM); //Draw the positions of the center of mass (extracted in dragEvent)
+               //of the track "1"
 }
 
 void ofApp::annotate( void ) {
@@ -85,7 +89,9 @@ void ofApp::windowResized( int w, int h ) {
 
 void ofApp::dragEvent( ofDragInfo dragInfo ) {
     
-    
+    //This event is still called when a track file is dragged in the application window.
+    // Here we extract the center of mass of the track (to display its trace it in the 2D layer)
+    COM = Geometry::centerOfMassTrace(track("1"));
 }
 
 void ofApp::gotMessage( ofMessage msg ) {
--- apps/examples/mmTutorial_3_TinyExample/src/ofApp.h
+++ apps/examples/mmTutorial_3_TinyExample/src/ofApp.h
@@ -34,4 +34,6 @@ class ofApp : public MoMa::SceneApp {
     void windowResized( int w, int h );
     void dragEvent( ofDragInfo dragInfo );
     void gotMessage( ofMessage msg );
+
+    MoMa::Trace COM;
 };
